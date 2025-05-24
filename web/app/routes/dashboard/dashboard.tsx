8237fd9950adca6272f6c2ccce4dade27ec8ae45
Adriein <adria.claret@gmail.com>
2025-05-24 19:34:10 +0200

    feat(#PI-9): setting dashboard

---
+++ web/app/routes/dashboard/dashboard.tsx
@@ -0,0 +1,9 @@
+import type {Route} from "../../../.react-router/types/app/routes/landing/+types/landing";
+
+export default function Dashboard({loaderData}: Route.ComponentProps) {
+    return (
+        <div>
+            Dashboard
+        </div>
+    );
+}
