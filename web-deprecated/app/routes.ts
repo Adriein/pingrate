55c53917c51155f7f8dae658dea15147f1abb7de
Adriein <adria.claret@gmail.com>
2025-06-07 17:22:56 +0200

    feat(#PI-9): using sessions instead of jwt

--- web-deprecated/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/landing/landing.tsx"),
-    route("/signup", "routes/signup/signup.tsx"),
-    route("/signin", "routes/signin/signin.tsx"),
-    route("/dashboard", "routes/dashboard/dashboard.tsx")
-] satisfies RouteConfig;
